307edd92e1502eb7195cc109ac0cd2bff6afa4ee
victorzarzar <victorzarzar58@gmail.com>
2024-01-04 12:08:31 -0300

    implement new function in task.form

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
     <div className="mt-10 h-24 p-8 pb-10 bg-[#282c34]">
       <footer className="mx-auto flex max-w-4xl flex-row">
         <ul className="flex items-center mx-auto flex-wrap mb-6 md:mb-0">
-          <li><p className="md:text-xs lg:text-lg font-normal ttext-gray-50 dark:text-gray-400  mr-4 md:mr-6"> © {currentYear} Todo List TS. All rights reserved.</p></li>
+          <li><p className="md:text-xs lg:text-lg font-normal text-[#61dafb]  mr-4 md:mr-6"> © {currentYear} Todo List TS. All rights reserved.</p></li>
         </ul>
       </footer>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import HomePage from "./components/Home"
+import { Task } from "./interfaces/Task"
 
 export default function Home() {
   return (
