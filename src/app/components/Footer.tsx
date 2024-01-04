8e90b95ec8f342be905dcf4b70d310aa6efe8efa
victorzarzar <victorzarzar58@gmail.com>
2024-01-04 08:40:33 -0300

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="mt-10 h-24 p-8 pb-10">
+      <footer className="mx-auto flex max-w-4xl flex-row">
+        <ul className="flex items-center mx-auto flex-wrap mb-6 md:mb-0">
+          <li><p className="md:text-xs lg:text-lg font-normal ttext-gray-50 dark:text-gray-400  mr-4 md:mr-6"> © {currentYear} Todo List TS. All rights reserved.</p></li>
+        </ul>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='header bg-[#282c34] text-center p-4 text-[#61dafb]'>
+            <h1>REACT + TS Todo List</h1>
+        </div>
+    )
+}
+
+export default Header
