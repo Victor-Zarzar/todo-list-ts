51120ea9c9bcf6fb5f364409294bef8bebe29be4
victorzarzar <victorzarzar58@gmail.com>
2024-01-04 14:45:07 -0300

    implement funcion taskform.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Taskform from './Taskform'
 import Tasklist from './Tasklist'
+import { Task } from '../interfaces/Task'
 
 function HomePage() {
+    
+    const [tasklist, setTaskList] = useState<Task[]>([])
+
     return (
         <>
             <div className="mt-40 text-center">
                 <h2 className='text-lg md:text-2xl mb-10 font-bold'>What will you do?</h2>
-                <Taskform btnText={'Create Task'} />
+                <Taskform btnText={'Create Task'} taskList={[]} />
             </div>
             <div className="mt-16 text-center">
                 <h2 className='text-lg md:text-2xl mb-6'>Your tasks:</h2>
--- src/app/components/Taskform.tsx
+++ src/app/components/Taskform.tsx
@@ -1,37 +1,40 @@
+'use client';
 import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import { Task } from "../interfaces/Task"
 
 type Props = {
     btnText: string
+    taskList: Task[]
+    setTaskList?: React.Dispatch<React.SetStateAction<Task[]>>
 }
 
-const Taskform = ({ btnText }: Props) => {
+const Taskform = ({ btnText, taskList, setTaskList }: Props) => {
 
     const [id, setId] = useState<number>(0);
     const [title, setTitle] = useState<string>("");
     const [difficulty, setDifficulty] = useState<number>(0)
 
-    function addTaskHandler() {
+    const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
+        const id = Math.floor(Math.random() * 1000)
+        const newTask: Task = { id, title, difficulty }
+
+        setTaskList!([...taskList, newTask])
+
+        setTitle("");
+        setDifficulty(0);
     }
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if(e.target.name === title)
-     {
-        setTitle(e.target.value)
-     } else {
-        setDifficulty
-     }
-    }
+        if (e.target.name === title) {
+            setTitle(e.target.value)
+        } else {
+            setDifficulty(parseInt(e.target.value))
+        }
 
-
-
-
-
-
-
-
-
+    };
+    
     return <form onSubmit={addTaskHandler} className='form flex flex-col max-w-96 m-0 mx-auto'>
         <div>
             <label htmlFor='title' className='mr-2 text-lg md:text-xl float-left'>Title:</label>
@@ -41,7 +44,7 @@ const Taskform = ({ btnText }: Props) => {
             <label htmlFor='difficulty' className='mr-2 text-lg md:text-xl float-left'>Difficulty:</label>
             <input type='text' name='difficulty' placeholder='Difficulty for task' onChange={handleChange} className='input p-2 mb-6 mt-4 border-2 bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-60 md:w-full'></input>
         </div>
-        <input type='submit' className='bg-[#61bafb] from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white w-60 md:w-full rounded-lg' value={btnText}></input>
+        <input type='submit' className='bg-[#61bafb] from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white w-60 md:w-full rounded-lg cursor-pointer' value={btnText}></input>
     </form>
 }
 
